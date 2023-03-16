41434e406542335a5dad870f0e62a40dba0d1031
NaomiYilma <yilmanaomi@gmail.com>
2023-03-16 04:13:32 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - main point
- *return - should end with the value 0 
+ * main - main point
+ * return - should end with the value (0) 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
