db2f338dc84d3c11d2fa77ecb5cb816191c31202
NaomiYilma <yilmanaomi@gmail.com>
2023-03-16 04:43:55 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+
  * Description: 'Printing using puts'
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
