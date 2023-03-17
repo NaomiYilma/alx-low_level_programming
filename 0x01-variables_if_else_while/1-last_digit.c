18429ff7e66a3873e94ede02a34d73bd7cb3a074
NaomiYilma <yilmanaomi@gmail.com>
2023-03-17 02:17:01 -0700

    0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'using if'
+ *
+ * Return: 'is positive' if n>0, 'is zero' if n=0, 'is negative' is n<0
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        printf("n>0 %s\n", "is positive")
+        return (0);
+}
