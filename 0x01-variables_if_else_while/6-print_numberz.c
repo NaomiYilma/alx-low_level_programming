ed5c97a35bf251b3e9083358e6235415135be6a0
NaomiYilma <yilmanaomi@gmail.com>
2023-03-17 14:37:40 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+/**
+ * main - using putchar
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n')
+	return (0);
