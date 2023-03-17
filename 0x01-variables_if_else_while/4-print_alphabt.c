1ce63a4716b6b75a37f758af3bf0382e5387f290
NaomiYilma <yilmanaomi@gmail.com>
2023-03-17 13:42:47 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints alphabet in lowercase 
+ * main - prints alphabet in lowercase
  *
  * Return: Always return 0
  */
@@ -8,7 +8,7 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch<= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
 		putchar(ch);
