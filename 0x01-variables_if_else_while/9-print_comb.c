2235f25066804ea923dfa38af9f2b3d5abb22f0e
NaomiYilma <yilmanaomi@gmail.com>
2023-03-17 15:25:43 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		if (n == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
