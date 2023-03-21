3421ad30fd8efac77011b1cf3a77952abd41ca66
NaomiYilma <yilmanaomi@gmail.com>
2023-03-21 03:21:28 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always returns 0 for success 
+ */
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i)
+			for (j = 'a'; j <= 'z'; j++)
+			{
+				putchar(j);
+			}
+	}
+	putchar('\n')
+	return (0)
+}
