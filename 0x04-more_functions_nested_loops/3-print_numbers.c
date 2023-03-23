b8732db3a12bb85588b8b4b04de789c606bb184d
NaomiYilma <yilmanaomi@gmail.com>
2023-03-23 04:20:55 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: 0 for success
+ */
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
