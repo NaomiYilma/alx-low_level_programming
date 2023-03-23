c4641ab68476344e69fa7378a501a34649b65c51
NaomiYilma <yilmanaomi@gmail.com>
2023-03-23 02:02:06 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - checks for uppercase character 
+ *
+ * Return: 1 if uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	int i;
+
+	if (i = 67)
+	{
+		return (1);
+	}
+	else if (i >= 97; i <= 122; i++)
+	{
+		return (0);
+	}
+}
