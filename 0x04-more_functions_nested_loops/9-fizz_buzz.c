cd7a58f9384f58ff8f96445898b28d644fa7feca
NaomiYilma <yilmanaomi@gmail.com>
2023-03-23 14:26:26 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-                {
+		{
 			printf("FizzBuzz");
-                }
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
