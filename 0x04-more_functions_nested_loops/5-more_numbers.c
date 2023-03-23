3285edd1208f147a9163987c912b85eec3b05422
NaomiYilma <yilmanaomi@gmail.com>
2023-03-23 08:09:33 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 0 to 14 ten times 
+ * more_numbers - prints 0 to 14 ten times
  *
  * Return: void
  */
 void more_numbers(void)
 {
 	int i;
-	int j = 0;
+	int j;
 
-	while (j <= '9')
-	for (i = '0'; i <= 14; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		i++;
-	}
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+		{
+			putchar ((j / 10) + '0');
+		}
+			putchar ((j % 10) + '0');
+		}
 	putchar('\n');
+	}
 }
