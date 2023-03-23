63ae76c26d0d3ce4be3c75cf1d4d272b1519a6dd
NaomiYilma <yilmanaomi@gmail.com>
2023-03-23 10:13:01 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
 	{
 		for (i = 0; i < n; i++)
 		{
 			putchar('_');
 		}
-	}
-	else
-	{
-		putchar('\n');
-	}
 	putchar('\n');
+	}
 }
