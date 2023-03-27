494fecc880265b0e761135f9fda4a011ec08a0a4
NaomiYilma <yilmanaomi@gmail.com>
2023-03-27 13:45:41 -0700

    0-3

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - value points to 98
+ * @n: dereferenced n
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string
+ *
+ * Return: A string
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
