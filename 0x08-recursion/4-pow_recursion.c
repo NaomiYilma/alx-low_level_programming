d3183d692263525b3770b87305db3945d9361f5d
NaomiYilma <yilmanaomi@gmail.com>
2023-04-05 03:38:24 -0700

    3-4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - the value of x to the power of y
+ * @x: given number x
+ * @y: given exponent of x
+ *
+ * Return: 0 for success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+	return (1);
+}
