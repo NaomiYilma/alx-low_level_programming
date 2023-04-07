d1009ee56f930c6ee3bb06d7ab1a98b8eb84f47b
NaomiYilma <yilmanaomi@gmail.com>
2023-04-07 02:39:35 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: Name of program
+ */
+int main(int argc, char **argv)
+{
+	while (argc == 0)
+	{
+		return (*argv[0]);
+	}
+	return (0);
+}
