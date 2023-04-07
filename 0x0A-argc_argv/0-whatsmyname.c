798617aec6fcbaad3f1310716a9442807218b106
NaomiYilma <yilmanaomi@gmail.com>
2023-04-07 03:44:44 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,8 @@
  *
  * Return: Name of program
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	while (argc == 0)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
