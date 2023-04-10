00ecfd38c20fc71eaf3cb9101fa9557d80362c65
NaomiYilma <yilmanaomi@gmail.com>
2023-04-10 10:29:26 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,18 +12,29 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int n = 0;
 	char *s3;
+	int length1 = 0;
+	int length2 = 0;
+	int length = length1 + length2;
+
+	s3 = malloc((length + 1) * sizeof(char));
 
 	while (s1[i] != '\0')
 	{
 		s3[n] = s1[i];
 		i++;
 		n++;
+		length1++;
 	}
-	while (s2[i] != '\0' && s3[i] != '\0')
+	while (s2[i] != '\0')
 	{
 		s3[n] = s2[i];
 		i++;
 		n++;
+		length2++;
+	}
+	while (s3[i] != '\0')
+	{
+		return (s3);
 	}
 	return (NULL);
 }
